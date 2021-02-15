69a3129a3d750c717badc62540fc28d9d4601272
Fabv19 <fabv19@hotmail.com>
2021-02-15 11:22:58 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,27 @@
 
 <?php
 
-// define variables and set to empty values
-$userIdErr = $idErr = $titleErr = $bodyErr = "";
-$userId = $id = $title = $comment = $body = "";
+echo "<table style='border: solid 1px #ddd;'>";
+ echo "<tr><th>UserId</th><th>ID</th><th>Title</th><th>Body</th></tr>";
 
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
 
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+  
 $servername = "localhost";
 $username = "admin";
 $password = "Monday15";
